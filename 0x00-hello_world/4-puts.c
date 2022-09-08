1d4d12f5f7605588af2c22371274c95f963990ec
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-08 08:41:58 -0700

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/*
+ *
+ *This is a program that only prints out a string literal
+ *
+ *
+ * 
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
