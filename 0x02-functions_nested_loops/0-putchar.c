4dd50d059fe218acd966ba37f0f14d0389d006c2
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-13 13:40:00 -0700

    A code that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include"main.h"
 /*
- *THis function prints _putchar
- *Return: 0
+ *main - THis function prints _putchar
+ *
+ *Return:Always 0 (Success)
  */
 int main(void)
 {
 	char o[] = "_putchar";
 	int c;
+
 	for(c=0;c<8;c++)
 	{
 		putchar(o[c]);
