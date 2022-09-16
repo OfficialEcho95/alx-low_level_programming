d099a85dea7cc13f6008ae9ec40e9797c30e5126
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-15 23:33:48 -0700

    My commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-*print_numbers - code to print for digits
+* print_numbers - code to print for digits
 *
-*Return: Always 0 (Success)
+* Return: Always 0.
 */
 void print_numbers(void)
 {
 	int c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
