ac3bd4afbbba3e45dd3c1c90ae28e11330e5bdb7
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-19 00:36:35 -0700

    My commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
 * reset_to_98 - check the code
-* @n - Int value for the argument of the functione
+*@n: Int value for the argument of the function
 * Return: void
 */
 void reset_to_98(int *n)
