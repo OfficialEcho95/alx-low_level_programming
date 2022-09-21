10cc8592af8d2d7f706d56597356fcc715505aa8
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-21 14:28:47 -0700

    Initial

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 /**
-*cap_string - capitalizes all words of a string.
+* cap_string - capitalizes all words of a string.
 * @s: pointer to input string.
 * Return: Returns pointer to capitalized string.
 */
