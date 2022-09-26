22435a349c0b37a98f1665e3301b2bb605ace20a
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-26 06:20:03 -0700

    Initial

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,9 +2,9 @@
 
 /**
 * _strstr - Locates a substring.
-*@haystack: The string to be searched.
-*@needle: The substring to be located.
-*Return: If the substring is located - a pointer to the beginning of the located substring.
+* @haystack: The string to be searched.
+* @needle: The substring to be located.
+* Return: If the substring is located - a pointer to the beginning of the located substring.
 * If the substring is not located - NULL.
 */
 
