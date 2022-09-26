ac4812819f5444614504f9475371b24b3b36efec
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-26 05:34:10 -0700

    Initial

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,7 +8,6 @@
 * Return: If the substring is located - a pointer to the beginning of the located substring.
 *         If the substring is not located - NULL.
 */
-
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
