392ac46a2d38409d30648d6108cd24fb188a69a0
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-26 06:07:53 -0700

    Initial

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,15 +7,13 @@
 * @n: number of bytes
 * Return: pointer to dest
 */
-
-char *_memcpy (char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
 	while (i < n)
 	{
 		dest[i] = src[i];
-
 		i++;
 	}
 	return (dest);
