95b5ed5b37819c5beab5871286e1eca3507ffd85
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-28 04:41:47 -0700

    Initial

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,7 +6,8 @@ char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
 
 /**
-* strlen_no_wilds - Returns the length of a string ignoring wildcard characters.
+* strlen_no_wilds - Returns the length of a
+* string ignoring wildcard characters
 * @str: The string to be measured.
 *
 * Return: The length.
@@ -28,7 +29,8 @@ int strlen_no_wilds(char *str)
 }
 
 /**
-* iterate_wild - Iterates through a string located at a wildcard until it points to a non-wildcard character.
+* iterate_wild - Iterates through a string located at a
+* wildcard until it points to a non-wildcard character.
 * @wildstr: The string to be iterated through.
 */
 void iterate_wild(char **wildstr)
@@ -41,11 +43,13 @@ void iterate_wild(char **wildstr)
 }
 
 /**
-* postfix_match - Checks if a string str matches the postfix of another string potentially containing wildcards.
+* postfix_match - Checks if a string str matches the
+* postfix of another string potentially containing wildcards.
 * @str: The string to be matched.
 * @postfix: The postfix.
 *
-* Return: If str and postfix are identical - a pointer to the null byte located at the end of postfix.
+* Return: If str and postfix are identical - a pointer to
+* the null byte located at the end of postfix.
 * Otherwise - a pointer to the first unmatched character in postfix.
 */
 char *postfix_match(char *str, char *postfix)
