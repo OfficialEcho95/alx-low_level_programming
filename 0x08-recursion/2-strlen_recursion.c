1c970c52cb6f18e40f0e38ff80fd392831ecad7f
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-28 01:27:22 -0700

    Initial

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strlen_recursion - prints a string length
+* @s: String argument
+*
+* Return: length
+*/
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	
+	if (s[i] == '\0')
+	{
+		return (0);
+	}
+	else 
+		return (1 + _strlen_recursion(s + 1));
+}
