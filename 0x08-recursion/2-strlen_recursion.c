210088f9c5a4b613999ea9ca646e93d62b9fcc90
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-28 02:14:49 -0700

    Initial

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,13 +10,11 @@
 int _strlen_recursion(char *s)
 {
 	unsigned int i = 0;
-	
+
 	if (s[i] == '\0')
 	{
 		return (0);
 	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
+
+	return (1 + _strlen_recursion(s + 1));
 }
