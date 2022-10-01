68631089182be57acf752bd0850c9e395820edac
Emmanuel Chukwu <officialecho95@outlook.com>
2022-09-30 22:15:21 -0700

    Initial

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _islower- that checks for alphabetic characters
+*@c: - is the input char
+*
+* Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
