2359860ceadfa943477c331feae253e9b18f2362
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-06 10:17:50 -0700

    Initial

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,10 +9,5 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _puts(const char *str);
 int main(int argc, char **argv);
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif
