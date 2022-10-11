7c5d01007f336ad5d38830f2a83deffb3223a03f
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-11 02:51:10 -0700

    Initial

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,29 +22,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 	for (nl = 0; name[nl]; nl++)
-		;
-	nl++;
+		nl++;
 	dog->name = malloc(nl * sizeof(char));
 	if (dog->name == NULL)
-		{
+	{
 		free(dog);
 		return (NULL);
-		}
+
 	for (i = 0; i < nl; i++)
-	dog->name[i] = name[i];
+		dog->name[i] = name[i];
 	dog->age = age;
 	for (ol = 0; owner[ol]; ol++)
 		;
-	ol++;
+		ol++;
 	dog->owner = malloc(ol * sizeof(char));
-	
-	if(dog->owner == NULL)
-		{
-		ree(dog->name);
-	free(dog):																						return (NULL);																										}
-	
-	
-	[for (i = 0; i < ol; i++)
-															dog->owner[i] = owner[i];
-																		return (dog);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
 }
