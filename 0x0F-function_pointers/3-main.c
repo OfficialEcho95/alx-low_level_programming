0b2e54e8c41a75f783f5bfcf41e0cb8255f11e3c
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-12 10:06:16 -0700

    Initial

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,21 +13,21 @@ int main(int argc, char *argv[])
 {
 	int a, b, answer;
 	int (*f)(int, int);
-			
+
 	if (argc != 4)
-	{		
-		printf("Error\n");	
-		exit(98);				
-	}			
-	a = atoi(argv[1]);	
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	f = get_op_func(argv[2]);				
-	if (f == NULL)				
-	{		
-		printf("Error\n");				
-		exit(99);				
-	}			
-	answer = f(a, b);			
-	printf("%d\n", answer);			
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	answer = f(a, b);
+	printf("%d\n", answer);
 	return (0);
 }
