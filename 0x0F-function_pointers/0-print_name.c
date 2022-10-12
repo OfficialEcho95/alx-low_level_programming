048b97626aa07231254d72e3e8f006416278b3b7
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-12 07:30:20 -0700

    Initial

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,10 @@
 #include "function_pointers.h"
 
  /**
- * print_name- Prints name
- * @f: The parameter
- * @name: The name to be printed
+ * print_name() - Prints a name
+ * @name: The parameter
+ * @f: The name to be printed
+ *
  */
 
 void print_name(char *name, void (*f)(char *))
