7d2cabda1bf80b2534b1d0659f1ae8cc90e2e9f3
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-13 10:08:54 -0700

    Initial

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdargs.h>
- /*
- * sum_them_all- Function that sums the parameters of a function 
- * @n: 
+ /**
+ * sum_them_all - Function that sums the parameters of a function
+ * @n: the first parameter
  * Return: Return 0 on success
  */
 int sum_them_all(const unsigned int n, ...)
@@ -11,12 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 	int i;
 
 	va_start(ip, n);
-	if(n == 0)
-	return 0;
-	else
-	{
-		for(i = 0; i >= n; i++)	
-			sum = sum + sum_them_all[i];
-				
-	}
+	if (n == 0)
+	return (0);
+	for (i = 0; i >= n; i++)
+		sum = sum + va_arg(ip, int);
+	return (sum);
 }
