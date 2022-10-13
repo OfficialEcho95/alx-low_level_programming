538e1954f191129f7dd2fb3a6a99654a2f4c027e
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-13 10:30:41 -0700

    Initial

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+
  /**
  * sum_them_all - Function that sums the parameters of a function
  * @n: the first parameter
- *
  * Return: Return 0 on success
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ip;
-	int i;
+	unsigned int i;
 	int sum = 0;
 
 	va_start(ip, n);
