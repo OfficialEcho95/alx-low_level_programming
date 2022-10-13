d5534a417810171de423d86f81801313b75a9bd4
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-13 10:18:49 -0700

    Initial

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,8 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	va_start(ip, n);
-	if (n == 0)
-	return (0);
+	
 	for (i = 0; i < n; i++)
 		sum = sum + va_arg(ip, int);
 	va_end(ip);
