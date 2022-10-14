cb812c4d2832b9c9995e0d2c5fc504feb79818be
Emmanuel Chukwu <officialecho95@outlook.com>
2022-10-13 21:55:43 -0700

    Initial

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,22 +15,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr;
 	char *str;
-	
+
 	va_start(ptr, n);
-	for (i = 0; i < n; i++)	
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ptr, char *);
-		if (str == NULL)					
-			printf("(nil)");				
+		if (str == NULL)
+			printf("(nil)");
 		else
 			printf("%s", str);
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 
 	}
-	
+
 	printf("\n");
 
 	va_end(ptr);
 }
-
