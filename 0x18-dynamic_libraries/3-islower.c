2566012aea2208a7fbd0a0bfa441a4679824045f
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-12 21:42:04 +0000

    Initial

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+/**
+*_islower- that checks for alphabetic characters
+*@c: - is the input char
+*
+* Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_abs - check the code for ALX School students.
+* @n: int argument for function
+* Return: Always 0
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (n * -1);
+}
