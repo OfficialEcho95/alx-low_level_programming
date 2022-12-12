df1f8d7a77c696fceffa880fae495df08f47df6e
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-12 18:08:15 +0000

    Initial

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+https://github.com/OfficialEcho95/alx-low_level_programming/blob/65430e03b4666da86e9f5125b93a89cc0d69ee32/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
