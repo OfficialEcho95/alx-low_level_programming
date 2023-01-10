72ec1acc8bfd8185b52636744807a9dcd1535337
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-10 06:48:28 +0000

    Initial

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
         unsigned long int i;
+
         hash_table_t* hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
         if (hash_table == NULL)
                 return (NULL);
